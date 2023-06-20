ad576656d2181821708e04c1eb40773c0baa0bc7
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-20 16:12:52 +0200

    Rename Routes.jsx en RoutesConfig.jsx

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import RoutesConfig from "./Routes";
+import RoutesConfig from "./RoutesConfig";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
