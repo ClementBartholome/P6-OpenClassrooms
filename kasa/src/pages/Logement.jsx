b55c2891bccf7285f64ffcc699da6e193b1f6be2
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-20 15:34:52 +0200

    feat: ajout du composant Logement

--- kasa/src/pages/Logement.jsx
+++ kasa/src/pages/Logement.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import logementsData from "../data/logements.json";
+import Collapse from "../components/Collapse";
 
 function Logement() {
+  const { logementId } = useParams();
+  const [logement, setLogement] = useState(null);
+
+  useState(() => {
+    const logementData = logementsData.find(
+      (logement) => logement.id === logementId
+    );
+
+    // The state logement is updated everytime the logementId changes in the URL
+    setLogement(logementData);
+  }, [logementId]);
+
+  if (!logement) {
+    return <div>Logement non trouvé</div>;
+  }
+
   return (
-    <div>
-      <h1>Logement</h1>
-    </div>
+    <section className="logement">
+      {/* Créer composant Carrousel */}
+      <img src={logement.cover} alt={logement.title} />
+      <h2>{logement.title}</h2>
+      <div className="logement-collapses">
+        <Collapse title="Description" content={logement.description} />
+        <Collapse title="Équipements" content={logement.equipments} />
+      </div>
+    </section>
   );
 }
 
