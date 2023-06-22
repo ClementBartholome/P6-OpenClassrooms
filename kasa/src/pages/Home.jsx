16d01ed58f864b9cb4193698019c533e6ef424ac
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-22 14:25:46 +0200

    refactor: utilisation d'axios au lieu d'importer la data

--- kasa/src/pages/Home.jsx
+++ kasa/src/pages/Home.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Banner from "../components/Banner";
 import HomeBanner from "../images/banner-home.png";
 import Card from "../components/Card";
-import data from "../data/logements.json";
 
 function Home() {
+  const [logements, setLogements] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/logements.json");
+        setLogements(response.data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Banner
         bannerImg={HomeBanner}
         bannerTitle="Chez vous, partout et ailleurs"
       />
-      <Card data={data} />
+      <section className="logements">
+        {logements.map((logement) => (
+          <Card key={logement.id} logement={logement} />
+        ))}
+      </section>
     </>
   );
 }
--- kasa/src/pages/Logement.jsx
+++ kasa/src/pages/Logement.jsx
@@ -1,30 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import logementsData from "../data/logements.json";
+import axios from "axios";
 import Collapse from "../components/Collapse";
 import Carrousel from "../components/Carrousel";
+import Tag from "../components/Tag";
 
 function Logement() {
   const { logementId } = useParams();
   const [logement, setLogement] = useState(null);
 
-  useState(() => {
-    const logementData = logementsData.find(
-      (logement) => logement.id === logementId
-    );
+  useEffect(() => {
+    const fetchLogement = async () => {
+      try {
+        const response = await axios.get("/logements.json");
+        const logementData = response.data.find(
+          (logement) => logement.id === logementId
+        );
+        setLogement(logementData);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données", error);
+      }
+    };
 
-    // The state "logement" is updated everytime the logementId changes in the URL
-    setLogement(logementData);
+    fetchLogement();
   }, [logementId]);
 
   if (!logement) {
     return <div>Logement non trouvé</div>;
   }
 
+  const tags = logement.tags;
+
   return (
     <section className="logement">
       <Carrousel images={logement.pictures} />
-      <h2>{logement.title}</h2>
+      <div className="logement-title">
+        <div>
+          <h2>{logement.title}</h2>
+          <p>{logement.location}</p>
+        </div>
+        <div className="host">
+          <p>{logement.host.name}</p>
+          <img src={logement.host.picture} alt={logement.host.name}></img>
+        </div>
+      </div>
+      <div className="tag-container">
+        {tags.map((tag) => (
+          <Tag key={tag} tagName={tag} />
+        ))}
+      </div>
       <div className="logement-collapses">
         <Collapse title="Description" content={logement.description} />
         <Collapse title="Équipements" content={logement.equipments} />
