193b905f53c8926f1f00f68794b771a07755da62
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-22 14:26:00 +0200

    refactor balises

--- kasa/src/components/Card.jsx
+++ kasa/src/components/Card.jsx
@@ -1,29 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import data from "../data/logements.json";
 
-function Card() {
-  const cardsList = data.map((logement) => {
-    const { cover, title, id } = logement;
-
-    return (
-      <li key={id}>
-        <NavLink to={`logement/${id}`}>
-          <figure className="card">
-            <img src={cover} alt={title} />
-            <figcaption>
-              <h2>{title}</h2>
-            </figcaption>
-          </figure>
-        </NavLink>
-      </li>
-    );
-  });
+function Card({ logement }) {
+  const { id, cover, title } = logement;
 
   return (
-    <section>
-      <ul className="logements">{cardsList}</ul>
-    </section>
+    <article key={id}>
+      <NavLink to={`logement/${id}`}>
+        <figure className="card">
+          <img src={cover} alt={title} />
+          <figcaption>
+            <h2>{title}</h2>
+          </figcaption>
+        </figure>
+      </NavLink>
+    </article>
   );
 }
 
